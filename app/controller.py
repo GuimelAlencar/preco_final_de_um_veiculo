a64e612e6e9065b594f3fd65c1fd9ea749ade7df
Guímel <guimel.alencar@proton.me>
2024-12-13 12:11:59 -0400

    Setting all up

---
+++ app/controller.py
@@ -0,0 +1,14 @@
+'''
+# Controle
+Arquivo responsável por gerenciar o relacionamento e o fluxo de informações entre a visualização, o processamento de dados, e a API da FIPE.
+'''
+def get_veicle_depreciation_by_FIPE(model, year):
+    pass
+
+def get_anual_fuel_cost(model):
+    pass
+
+def get_anual_tax_burden(model, year):
+    pass
+
+def 
---
+++ app/view.py
@@ -0,0 +1,7 @@
+'''
+# Visualização
+Arquivo responsável por gerar a interação entre o usuário e o programa por meio de alguma 
+biblioteca como StreamLit, Python kivy ou Plotly Dash
+'''
+
+from model import get_veicle_depreciation_by_FIPE, get_anual_fuel_cost, get_anual_tax_burden
