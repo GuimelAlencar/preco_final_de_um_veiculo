47da2a133fd2f101e2d73ee2c076d37fefe583af
Guímel de Alencar <guimel.alencar.s@gmail.com>
2024-12-16 16:37:33 -0400

    model, controller e db atualizadas

--- app/controller.py
+++ app/controller.py
@@ -2,13 +2,45 @@
 # Controle
 Arquivo responsável por gerenciar o relacionamento e o fluxo de informações entre a visualização, o processamento de dados, e a API da FIPE.
 '''
-def get_veicle_depreciation_by_FIPE(model, year):
-    pass
+from model import valor_veiculo
+from model import calculo_depreciacao
+from model import calculo_financiamento
 
-def get_anual_fuel_cost(model):
-    pass
+def depreciacao(
+        tipo_veiculo, 
+        marca_veiculo, 
+        modelo_veiculo, 
+        ano_veiculo
+    ):
+    dados_depreciacao = calculo_depreciacao(
+        tipo_veiculo, 
+        marca_veiculo, 
+        modelo_veiculo, 
+        ano_veiculo,
+    )
+    return dados_depreciacao 
 
-def get_anual_tax_burden(model, year):
-    pass
-
-def 
+def financiamento(
+        tipo_veiculo, 
+        marca_veiculo, 
+        modelo_veiculo, 
+        ano_veiculo, 
+        taxa_de_juros, 
+        numero_de_parcela,
+        tipo_de_parcela="mensal", 
+        entrada=None,
+    ):
+    valor_veiculo_financiado = valor_veiculo(
+        tipo_veiculo, 
+        marca_veiculo, 
+        modelo_veiculo, 
+        ano_veiculo,
+    )
+    dados_financiamento = calculo_financiamento(
+        valor_veiculo_financiado,
+        taxa_de_juros, 
+        numero_de_parcela,
+        tipo_de_parcela, 
+        entrada,
+    )
+    return dados_financiamento
